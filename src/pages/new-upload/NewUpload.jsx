647ef2a0f7fe0160201015c9a0946e823bdf6d62
StefanPristoleanu <StefanPristoleanu@rentalcars.com>
2020-04-26 12:52:15 +0100

    Added second pdf and refactoring

--- src/pages/new-upload/NewUpload.jsx
+++ src/pages/new-upload/NewUpload.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import HeaderBar from '../../components/Molecules/header-bar/HeaderBar';
+import FooterBar from '../../components/Molecules/footer-bar/FooterBar';
 import PDFViewer from '../../components/Organisms/pdf-viewer/PDFViewer';
+import RatingButtons from '../../components/Atoms/rating-buttons/RatingButtons';
 
 export const NewUpload = () => {
   const dispatch = useDispatch();
   dispatch({
     type: 'TRIGGER_INITIAL_STATE',
   });
-  const ColoredLine = ({ color }) => (
-    <hr
-      style={{
-            color,
-            backgroundColor: color,
-            height: 0.5
-        }}
-    />
-);
   
   return (
     <div>
       <HeaderBar />
-      <ColoredLine color="gray" />
-      <PDFViewer path='./Document_Medical_Stef2.pdf' />
+      <br />
+      <PDFViewer path='./SURGICON.pdf' />
+      <RatingButtons />
+      <FooterBar />
     </div>
   );
 };
